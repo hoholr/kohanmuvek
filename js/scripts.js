76036a346520571d10d93ce9c35fb2b22bc57ac1
hoholr <hoholr@proton.me>
2026-05-05 18:22:14 +0200

    v0.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+$(".modal.show").modal('show');
+
+function villanyShow(){
+	$(".toggle.klima").hide();
+	$(".toggle.villany").show();
+	$(".tgl-btn-villany").addClass("d-none").removeClass("d-flex");
+	$(".tgl-btn-klima").addClass("d-flex").removeClass("d-none");
+}
+
+function klimaShow(){
+	$(".toggle.villany").hide();
+	$(".toggle.klima").show();
+	$(".tgl-btn-klima").addClass("d-none").removeClass("d-flex");
+	$(".tgl-btn-villany").addClass("d-flex").removeClass("d-none");
+}
+
 console.log("Csá OP");
